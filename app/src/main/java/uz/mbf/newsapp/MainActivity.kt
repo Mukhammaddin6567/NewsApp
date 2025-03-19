12c6767be76e698e99afb564d5521145f17c5ac0
Muhammaddin Baxridinov <mukhammaddinbakhridinov@gmail.com>
2025-03-19 16:05:32 +0500

    added core module

--- app/src/main/java/uz/mbf/newsapp/MainActivity.kt
+++ app/src/main/java/uz/mbf/newsapp/MainActivity.kt
@@ -1,23 +1,10 @@
 package uz.mbf.newsapp
 
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import uz.mbf.newsapp.ui.theme.NewsAppTheme
-import uz.mbf.newsapp.ui.theme.colors
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
