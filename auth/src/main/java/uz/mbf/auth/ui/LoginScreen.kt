98ff5e60ade1bea9b669b04364bf1ae54f4099dc
Muhammaddin Baxridinov <mukhammaddinbakhridinov@gmail.com>
2025-04-03 18:48:31 +0500

    added LoginScreen

---
+++ auth/src/main/java/uz/mbf/auth/ui/LoginScreen.kt
@@ -0,0 +1,24 @@
+package uz.mbf.auth.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import uz.mbf.coreui.theme.NewsAppTheme
+
+@Composable
+fun LoginScreen() {
+
+}
+
+@Composable
+private fun LoginView() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = NewsAppTheme.appColors.background)
+    ) {
+
+    }
+}
