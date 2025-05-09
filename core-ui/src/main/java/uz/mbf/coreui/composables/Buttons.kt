7582b8185b68a11b5a1cd7b59ec32ac939b70fdf
Muhammaddin Baxridinov <mukhammaddinbakhridinov@gmail.com>
2025-05-09 10:51:39 +0500

    added checkbox

--- core-ui/src/main/java/uz/mbf/coreui/composables/Buttons.kt
+++ core-ui/src/main/java/uz/mbf/coreui/composables/Buttons.kt
@@ -1,7 +1,11 @@
 package uz.mbf.coreui.composables
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import uz.mbf.coreui.theme.NewsAppTheme
@@ -14,7 +18,7 @@ fun PrimaryButton(
     onClick: () -> Unit
 ) {
     Button(
-        modifier = Modifier,
+        modifier = modifier.fillMaxWidth(),
         shape = NewsAppTheme.appShape.button,
         colors = ButtonDefaults.buttonColors(containerColor = NewsAppTheme.appColors.primary),
         onClick = onClick,
@@ -24,6 +28,23 @@ fun PrimaryButton(
                 color = NewsAppTheme.appColors.white
             )
         }
+    )
+}
+
+@Composable
+fun CheckBox(
+    modifier: Modifier = Modifier,
+    checked: Boolean,
+    onCheckedChange: () -> Unit
+) {
+    Checkbox(
+        modifier = Modifier.size(NewsAppTheme.appSize.size24dp),
+        checked = checked,
+        onCheckedChange = { checked.not() },
+        colors = CheckboxDefaults.colors(
+            checkedColor = NewsAppTheme.appColors.primary,
+//            uncheckedColor = NewsAppTheme.appColors.
+        ),
     )
 }
 
