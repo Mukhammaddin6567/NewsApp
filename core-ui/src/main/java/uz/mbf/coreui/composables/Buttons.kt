11b42a784da1e0f5a8055f9b8dc72d393e43faff
Muhammaddin Baxridinov <mukhammaddinbakhridinov@gmail.com>
2025-05-12 16:40:10 +0500

    added BackButton

--- core-ui/src/main/java/uz/mbf/coreui/composables/Buttons.kt
+++ core-ui/src/main/java/uz/mbf/coreui/composables/Buttons.kt
@@ -1,13 +1,21 @@
 package uz.mbf.coreui.composables
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import uz.mbf.coreui.theme.NewsAppTheme
 import uz.mbf.coreui.utils.AppPreview
 
@@ -48,10 +56,38 @@ fun CheckBox(
     )
 }
 
+@Composable
+fun BackButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+    IconButton(
+        modifier = Modifier
+            .size(24.dp)
+            .then(modifier),
+        onClick = onClick
+    ) {
+        Icon(
+            imageVector = Icons.AutoMirrored.Default.ArrowBack,
+            contentDescription = null,
+            tint = NewsAppTheme.appColors.bodyText
+        )
+    }
+}
+
 @AppPreview
 @Composable
 private fun MyPreview() {
     NewsAppTheme {
-        PrimaryButton(onClick = {}, text = "Hello World")
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(NewsAppTheme.appColors.background)
+        ) {
+            BackButton { }
+        }
+//        PrimaryButton(onClick = {}, text = "Hello World")
+
+
     }
 }
